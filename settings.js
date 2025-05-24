25712a07ed4536b853eb4b1d61dc6ca23e91b781
hole-254 <hole87779@gmail.com>
2025-05-24 12:25:25 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: 'stuber🥂', // Your name
   ownerNumber: '254712889487', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: "Public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.1",
 };
